b4881e8c0a7b73531aa102f90f44308983dcdbad
Owen Shaule <owen.shaule@gmail.com>
2024-04-27 16:31:34 -0400

    Reshuffle things

--- src/main/kotlin/coralis/utils/RobotState.kt
+++ src/main/kotlin/coralis/utils/RobotState.kt
@@ -15,9 +15,9 @@ class RobotState {
         get() {
             if (RobotState.isEStopped()) return State.ESTOPED
             else if (RobotState.isDisabled()) return State.DISABLED
+            else if (RobotState.isTest()) return State.TEST
             else if (RobotState.isAutonomous()) return State.AUTONOMOUS
             else if (RobotState.isTeleop()) return State.TELEOP
-            else if (RobotState.isTest()) return State.TEST
             else return State.DISABLED
         }
 }
